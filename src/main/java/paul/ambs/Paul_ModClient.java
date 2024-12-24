e0356fc2086163ccfbf445625f73e06881763419
SunPaul01 <paul.ambs@web.de>
2024-12-24 15:35:39 +0100

    Whuhuu

---
+++ src/main/java/paul/ambs/Paul_ModClient.java
@@ -0,0 +1,10 @@
+package paul.ambs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Paul_ModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
