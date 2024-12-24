62904f874f329381ca6660c1e9e387534d4727c1
SunPaul01 <paul.ambs@web.de>
2024-12-24 16:17:35 +0100

    New Class Items und update in Overworld.java

---
+++ src/main/java/paul/ambs/Item/ModItems.java
@@ -0,0 +1,10 @@
+package paul.ambs.Item;
+
+import paul.ambs.Paul_Mod;
+
+public class ModItems {
+
+    public static void registerModItems() {
+        Paul_Mod.LOGGER.info("Register Mod Items for " + Paul_Mod.MOD_ID);
+    }
+}
--- src/main/java/paul/ambs/World2/Overworld.java
+++ src/main/java/paul/ambs/World2/Overworld.java
@@ -1,4 +1,10 @@
 package paul.ambs.World2;
 
+import paul.ambs.Paul_Mod;
+
 public class Overworld {
+
+    public static void registerModWorlds() {
+        Paul_Mod.LOGGER.info("Register Mod Worlds for " + Paul_Mod.MOD_ID);
+    }
 }
